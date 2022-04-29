e09f74f22a8a263c04856769ced78a5f2252e7d0
reidlo5135 <201840103@email.daelim.ac.kr>
2022-04-29 16:32:20 +0900

    04/29 set WebView

--- app/src/main/java/com/daelim/main/InitActivity.java
+++ app/src/main/java/com/daelim/main/InitActivity.java
@@ -1,19 +1,21 @@
 package com.daelim.main;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.daelim.R;
 import com.daelim.calculate.CalActivity;
 import com.daelim.dialog.SharedPreferenceActivity;
 import com.daelim.research.ResearchActivity;
+import com.daelim.web.WebActivity;
 
 public class InitActivity extends AppCompatActivity {
 
@@ -28,6 +30,9 @@ public class InitActivity extends AppCompatActivity {
     private Button btn_survey;
     private Button btn_list;
     private Button btn_shared;
+    private Button btn_call;
+    private Button btn_email;
+    private Button btn_web;
 
     View.OnClickListener click = new View.OnClickListener() {
         @Override
@@ -51,6 +56,20 @@ public class InitActivity extends AppCompatActivity {
                 case R.id.btn_shared:
                     Intent it4 = new Intent(activity, SharedPreferenceActivity.class);
                     startActivity(it4);
+                    break;
+                case R.id.btn_call:
+                    Intent it5 = new Intent(Intent.ACTION_DIAL);
+                    it5.setData(Uri.parse("tel:01048542023"));
+                    startActivity(it5);
+                    break;
+                case R.id.btn_email:
+                    Intent it6 = new Intent(Intent.ACTION_VIEW);
+                    it6.setData(Uri.parse("http://www.naver.com"));
+                    startActivity(it6);
+                    break;
+                case R.id.btn_web:
+                    Intent it7 = new Intent(activity, WebActivity.class);
+                    startActivity(it7);
                     break;
             }
         }
@@ -77,6 +96,15 @@ public class InitActivity extends AppCompatActivity {
 
         btn_shared = findViewById(R.id.btn_shared);
         btn_shared.setOnClickListener(click);
+
+        btn_call = findViewById(R.id.btn_call);
+        btn_call.setOnClickListener(click);
+
+        btn_email = findViewById(R.id.btn_email);
+        btn_email.setOnClickListener(click);
+
+        btn_web = findViewById(R.id.btn_web);
+        btn_web.setOnClickListener(click);
 
         tv_data = findViewById(R.id.tv_data);
         Intent intent = new Intent();
